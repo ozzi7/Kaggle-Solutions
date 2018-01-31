8239035e4711df27c29941b79007563c03f88c2a
Pascal Widmer <pawiddy@gmail.com>
2018-01-31 22:40:26 +0100

    Update

---
+++ Tutorial/model-validation.py
@@ -0,0 +1,31 @@
+# Data Loading Code Hidden Here
+import pandas as pd
+
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import train_test_split
+
+# Load data
+melbourne_file_path = '../input/melbourne-housing-snapshot/melb_data.csv'
+melbourne_data = pd.read_csv(melbourne_file_path) 
+# Filter rows with missing values
+filtered_melbourne_data = melbourne_data.dropna(axis=0)
+# Choose target and predictors
+y = filtered_melbourne_data.Price
+melbourne_predictors = ['Rooms', 'Bathroom', 'Landsize', 'BuildingArea', 
+                        'YearBuilt', 'Lattitude', 'Longtitude']
+X = filtered_melbourne_data[melbourne_predictors]
+
+# split data into training and validation data, for both predictors and target
+# The split is based on a random number generator. Supplying a numeric value to
+# the random_state argument guarantees we get the same split every time we
+# run this script.
+train_X, val_X, train_y, val_y = train_test_split(X, y,random_state = 0)
+# Define model
+melbourne_model = DecisionTreeRegressor()
+# Fit model
+melbourne_model.fit(train_X, train_y)
+
+# get predicted prices on validation data
+val_predictions = melbourne_model.predict(val_X)
+print(mean_absolute_error(val_y, val_predictions))
