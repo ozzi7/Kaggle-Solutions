fabc66c6a664f9302d7e8b4f03357da533d7ce8a
Pascal Widmer <pawiddy@gmail.com>
2018-01-31 23:25:15 +0100

    Update

---
+++ Tutorial/submitting-from-a-kernel.py
@@ -0,0 +1,34 @@
+# Data Loading Code Runs At This Point
+import pandas as pd
+    
+from sklearn.metrics import mean_absolute_error
+from sklearn.tree import DecisionTreeRegressor
+
+# Load data
+melbourne_file_path = '../input/melbourne-housing-snapshot/melb_data.csv'
+melbourne_data = pd.read_csv(melbourne_file_path) 
+# Filter rows with missing values
+filtered_melbourne_data = melbourne_data.dropna(axis=0)
+# Choose target and predictors
+y = filtered_melbourne_data.Price
+melbourne_predictors = ['Rooms', 'Bathroom', 'Landsize', 'BuildingArea', 
+                        'YearBuilt', 'Lattitude', 'Longtitude']
+X = filtered_melbourne_data[melbourne_predictors]
+
+from sklearn.model_selection import train_test_split
+
+# split data into training and validation data, for both predictors and target
+train_X, val_X, train_y, val_y = train_test_split(X, y,random_state = 0)
+
+
+def get_mae(max_leaf_nodes, predictors_train, predictors_val, targ_train, targ_val):
+    model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes, random_state=0)
+    model.fit(predictors_train, targ_train)
+    preds_val = model.predict(predictors_val)
+    mae = mean_absolute_error(targ_val, preds_val)
+    return(mae)
+	
+# compare MAE with differing values of max_leaf_nodes
+for max_leaf_nodes in [5, 50, 500, 5000]:
+    my_mae = get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y)
+    print("Max leaf nodes: %d  \t\t Mean Absolute Error:  %d" %(max_leaf_nodes, my_mae))
