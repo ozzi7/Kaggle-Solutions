d93afd94016f9d53eadc074d91c6d871f10719a1
Pascal Widmer <pawiddy@gmail.com>
2018-02-05 21:35:11 +0100

    Update

--- Tutorial/submitting-from-a-kernel.py
+++ Tutorial/submitting-from-a-kernel.py
@@ -1,34 +1,29 @@
-# Data Loading Code Runs At This Point
+import numpy as np
 import pandas as pd
-    
-from sklearn.metrics import mean_absolute_error
-from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
 
-# Load data
-melbourne_file_path = '../input/melbourne-housing-snapshot/melb_data.csv'
-melbourne_data = pd.read_csv(melbourne_file_path) 
-# Filter rows with missing values
-filtered_melbourne_data = melbourne_data.dropna(axis=0)
-# Choose target and predictors
-y = filtered_melbourne_data.Price
-melbourne_predictors = ['Rooms', 'Bathroom', 'Landsize', 'BuildingArea', 
-                        'YearBuilt', 'Lattitude', 'Longtitude']
-X = filtered_melbourne_data[melbourne_predictors]
+# Read the data
+train = pd.read_csv('../input/train.csv')
 
-from sklearn.model_selection import train_test_split
+# pull data into target (y) and predictors (X)
+train_y = train.SalePrice
+predictor_cols = ['LotArea', 'OverallQual', 'YearBuilt', 'TotRmsAbvGrd']
 
-# split data into training and validation data, for both predictors and target
-train_X, val_X, train_y, val_y = train_test_split(X, y,random_state = 0)
+# Create training predictors data
+train_X = train[predictor_cols]
 
+my_model = RandomForestRegressor()
+my_model.fit(train_X, train_y)
 
-def get_mae(max_leaf_nodes, predictors_train, predictors_val, targ_train, targ_val):
-    model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes, random_state=0)
-    model.fit(predictors_train, targ_train)
-    preds_val = model.predict(predictors_val)
-    mae = mean_absolute_error(targ_val, preds_val)
-    return(mae)
-	
-# compare MAE with differing values of max_leaf_nodes
-for max_leaf_nodes in [5, 50, 500, 5000]:
-    my_mae = get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y)
-    print("Max leaf nodes: %d  \t\t Mean Absolute Error:  %d" %(max_leaf_nodes, my_mae))
+# Read the test data
+test = pd.read_csv('../input/test.csv')
+# Treat the test data in the same way as training data. In this case, pull same columns.
+test_X = test[predictor_cols]
+# Use the model to make predictions
+predicted_prices = my_model.predict(test_X)
+# We will look at the predicted prices to ensure we have something sensible.
+print(predicted_prices)
+
+my_submission = pd.DataFrame({'Id': test.Id, 'SalePrice': predicted_prices})
+# you could use any filename. We choose submission here
+my_submission.to_csv('submission.csv', index=False)
